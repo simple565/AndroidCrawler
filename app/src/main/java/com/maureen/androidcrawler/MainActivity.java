f577d25fe13d9fa0278c8c91afd3c6bd7a55086f
lianml <467914885@qq.com>
2020-04-09 19:49:50 +0800

    提交修改

--- app/src/main/java/com/maureen/androidcrawler/MainActivity.java
+++ app/src/main/java/com/maureen/androidcrawler/MainActivity.java
@@ -2,6 +2,8 @@ package com.maureen.androidcrawler;
 
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -21,18 +23,55 @@ import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
     private String TAG = MainActivity.class.getSimpleName();
+    private TextView mTextView;
+    private StringBuilder mStringBuilder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mTextView = findViewById(R.id.main_tv);
+        loadAllData();
+    }
+
+    public void loadData() {
+        Observable.create((ObservableOnSubscribe<String>) emitter -> {
+            String result = Objects.requireNonNull(WebClient.getInstance().syncGet(UrlConstants.SIGN_DATA_URL).body()).string();
+            if (TextUtils.isEmpty(result)) {
+                emitter.onError(new Throwable("数据为空"));
+            } else {
+                emitter.onNext(result);
+            }
+        }).subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<String>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(String s) {
+                        Log.d(TAG, "onNext: " + s);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.d(TAG, "onError: " + e.getMessage());
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
     }
 
     private void loadAllData() {
         Observable<String> signDataObservable = Observable.create(new ObservableOnSubscribe<String>() {
             @Override
             public void subscribe(ObservableEmitter<String> emitter) throws Exception {
-                String result = Objects.requireNonNull(WebClient.getInstance().syncGet(UrlConstants.SIGN_DATA_URL).body()).toString();
+                String result = Objects.requireNonNull(WebClient.getInstance().syncGet(UrlConstants.SIGN_DATA_URL).body()).string();
                 if (TextUtils.isEmpty(result)) {
                     emitter.onError(new Throwable("数据为空"));
                 } else {
@@ -44,12 +83,13 @@ public class MainActivity extends AppCompatActivity {
         Observable<String> popularityObservable = Observable.create(new ObservableOnSubscribe<String>() {
             @Override
             public void subscribe(ObservableEmitter<String> emitter) throws Exception {
-                String result = Objects.requireNonNull(WebClient.getInstance().syncGet(UrlConstants.POPULARITY_DATA_URL).body()).toString();
+                String result = Objects.requireNonNull(WebClient.getInstance().syncGet(UrlConstants.POPULARITY_DATA_URL).body()).string();
                 if (TextUtils.isEmpty(result)) {
                     emitter.onError(new Throwable("数据为空"));
                 } else {
                     emitter.onNext(result);
                 }
+                emitter.onComplete();
             }
         });
 
@@ -59,12 +99,13 @@ public class MainActivity extends AppCompatActivity {
                 .subscribe(new Observer<String>() {
                     @Override
                     public void onSubscribe(Disposable d) {
-
+                        mStringBuilder = new StringBuilder();
                     }
 
                     @Override
                     public void onNext(String s) {
-
+                        Log.d(TAG, "onNext: " + s);
+                        mStringBuilder.append(s);
                     }
 
                     @Override
@@ -74,7 +115,7 @@ public class MainActivity extends AppCompatActivity {
 
                     @Override
                     public void onComplete() {
-
+                        mTextView.setText(mStringBuilder.toString());
                     }
                 });
     }
